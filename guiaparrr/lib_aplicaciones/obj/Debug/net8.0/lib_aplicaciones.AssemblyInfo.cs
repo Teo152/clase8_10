3090abb3e07993d1922fb3bfb3ab387b5ea3b899
Teo152 <mateobedoyabedoya82@gmail.com>
2025-05-17 14:30:22 -0500

    se creo 2 clases

--- guiaparrr/lib_aplicaciones/obj/Debug/net8.0/lib_aplicaciones.AssemblyInfo.cs
+++ guiaparrr/lib_aplicaciones/obj/Debug/net8.0/lib_aplicaciones.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lib_aplicaciones")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+261a1d9d57259687b8d8977effe5cb05dc8da220")]
 [assembly: System.Reflection.AssemblyProductAttribute("lib_aplicaciones")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lib_aplicaciones")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
